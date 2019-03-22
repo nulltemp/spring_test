de86d43d42605d36c9c1529f46c0015040680a8f
nulltemp <nulltemp999@gmail.com>
2019-03-23 00:12:20 +0900

    api以下のURLに対するリクエストのみCSRF対策を無視するように修正

--- src/main/java/com/nulltemp/config/WebSecurityConfig.java
+++ src/main/java/com/nulltemp/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.nulltemp.config;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import com.nulltemp.constant.UrlConstant;
 
@@ -19,7 +23,23 @@ import com.nulltemp.constant.UrlConstant;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		RequestMatcher csrfRequestMatcher = new RequestMatcher() {
+			private AntPathRequestMatcher disabledRequestMatcher = new AntPathRequestMatcher(
+					UrlConstant.URL_API_BASE + "/**");
+
+			@Override
+			public boolean matches(HttpServletRequest request) {
+				if ("GET".equals(request.getMethod())) {
+					return false;
+				}
+				if (disabledRequestMatcher.matches(request)) {
+					return false;
+				}
+				return true;
+			}
+		};
+
+		http.csrf().requireCsrfProtectionMatcher(csrfRequestMatcher).and()
 				.authorizeRequests()
 				.antMatchers("/", "/home").permitAll()
 				.mvcMatchers(UrlConstant.URL_API_BASE + "/**").permitAll()
